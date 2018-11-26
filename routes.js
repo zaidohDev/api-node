3761fbb76e2d06ddd896acfdae8f78c7bfda8ffe
zaidoh2015 <danfealmeida@example.com>
2018-11-26 15:23:35 -0300

    update message

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 // Este arquivo define cada ponto de entrada dentro da aplicação
-import hello from './hello/index'
+import message from './message/index'
 import bills from './bills/index'
 import users from './users/index'
 import blogs from './blogs/index' 
 
 export default (app) => {
-    app.use('/', hello )
+    app.use('/', message )
     app.use('/bills', bills)
     app.use('/users', users)
     app.use('/blogs', blogs)
